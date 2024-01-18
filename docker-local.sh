6a99e5c69df7772f5ba06881f22c71c12f111f85
arun-ms-deriv <arun.ms@deriv.com>
2024-01-18 23:42:06 +0400

    Code Formatting

--- docker-local.sh
+++ docker-local.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 
 
-###################################################################################################################################################################################
-#                                                                                                                                                                                 #
-# Description       : This script serves the purpose of building, and pushing Docker Images.                                                                                      #
-# Author            : Team DevOps (Kubernetes)                                                                                                                                    #
-# Version           : 2024.01.001                                                                                                                                                 #
-# Created           : 18/Jan/2024                                                                                                                                                 #
-# Last Update       : Jan 2024                                                                                                                                                    #
-# Script Location   : https://github.com/arun-ms-deriv/dockerfile-action/blob/main/docker-local.sh                                                                                #
-# WikiJs Link       : <Need to update later>                                                                                                                                                            #
-#                                                                                                                                                                                 #
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-#                                                                                                                                                                                 #
-# Syntax : ./docker-local.sh [-di|--docker-image <Docker_Image>] [-du|--docker-username <Docker_Username>] [-dp|--docker-password <Docker_Password>] \                           #
-#                            [-df|--docker-file) <Docker_File>] [-ds|--docker-scan <true/false>] [-dpu|--docker-push] <true/false>] \                                           #
-#                            [-dc|--docker-context <test_path>] [-dc|--project-type <perl/python/go/node/skip>] [-f|--force]                                                                 #
-#                                                                                                                                                                                 #
-#                            -di (or) --docker-image     # Docker full image name                                                                                                 #
-#                            -du (or) --docker-username  # username used to login against the Docker registry                                                                     #
-#                            -dp (or) --docker-password  # Password or personal access token used to login against the Docker registry                                            #
-#                            -df (or) --docker-file      # Path to the Dockerfile                                                                                                 #
-#                            -ds (or) --docker-scan      # Boolean if we want to Scan the Docker image. Default: false                                                            #
-#                            -dpu(or) --docker-push      # Boolean to represent if we want to Push image to a Docker registry                                                     #
-#                            -dc (or) --docker-context   # Build's context is the set of files located in the specified PATH or URL                                               #
-#                            -pt (or) --project-type     # Type of the project: python, node, go or perl. Use skip to ignore this check                                           #
-#                            -f  (or) --force            # Force with no-prompt                                                                                                   #
-#                                                                                                                                                                                 #
-# Example   : ./docker-local.sh -di testimage:latest -du docker_username -dp ******* -df ./Dockerfile -ds false -dpu false -dc ./context.txt -pt skip --force                     #
-#                                                                                                                                                                                 #
-###################################################################################################################################################################################
+#############################################################################################################################################################################################
+#                                                                                                                                                                                           #
+# Description       : This script serves the purpose of building, and pushing Docker Images.                                                                                                #
+# Author            : Team DevOps (Kubernetes)                                                                                                                                              #
+# Version           : 2024.01.001                                                                                                                                                           #
+# Created           : 18/Jan/2024                                                                                                                                                           #
+# Last Update       : Jan 2024                                                                                                                                                              #
+# Script Location   : https://github.com/arun-ms-deriv/dockerfile-action/blob/main/docker-local.sh                                                                                          #
+# WikiJs Link       : <Need to update later>                                                                                                                                                #
+#                                                                                                                                                                                           #
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#                                                                                                                                                                                           #
+# Syntax : ./docker-local.sh [-di|--docker-image <Docker_Image>] [-du|--docker-username <Docker_Username>] [-dp|--docker-password <Docker_Password>] \                                      #
+#                            [-df|--docker-file) <Docker_File>] [-ds|--docker-scan <true/false>] [-dpu|--docker-push] <true/false>] \                                                       #
+#                            [-dc|--docker-context <test_path>] [-dc|--project-type <perl/python/go/node/skip>] [-f|--force]                                                                #
+#                                                                                                                                                                                           #
+#                            -di (or) --docker-image     # Docker full image name (required)                                                                                                #
+#                            -du (or) --docker-username  # username used to login against the Docker registry (optional / required if in case of push set to true)                          #
+#                            -dp (or) --docker-password  # Password or personal access token used to login against the Docker registry (optional / required if in case of push set to true) #
+#                            -df (or) --docker-file      # Path to the Dockerfile (required)                                                                                                #
+#                            -ds (or) --docker-scan      # Boolean if we want to Scan the Docker image. Default: false (optional)                                                           #
+#                            -dpu(or) --docker-push      # Boolean to represent if we want to Push image to a Docker registry Default: false (optional)                                     #
+#                            -dc (or) --docker-context   # Build's context is the set of files located in the specified PATH or URL (optional)                                              #
+#                            -pt (or) --project-type     # Type of the project: python, node, go or perl. Use skip to ignore this check (optional)                                          #
+#                            -f  (or) --force            # Force with no-prompt (optional)                                                                                                  #
+#                            -d  (or) --debug            # Enable Debug Mode (optional)                                                                                                     #
+#                                                                                                                                                                                           #
+# Example   : ./docker-local.sh -di testimage:latest -du docker_username -dp ******* -df ./Dockerfile -ds false -dpu false -dc ./context.txt -pt skip --force                               #
+#                                                                                                                                                                                           #
+#############################################################################################################################################################################################
 
 #****************************************************************** Start of Script ********************************************************************#
 
@@ -113,7 +114,8 @@ input_usage() {
                 -dpu(or) --docker-push      # Boolean to represent if we want to Push image to a Docker registry
                 -dc (or) --docker-context   # Build's context is the set of files located in the specified PATH or URL
                 -pt (or) --project-type     # Type of the project: python, node, go or perl. Use skip to ignore this check
-                -f  (or) --force            # Force with no-prompt\n"
+                -f  (or) --force            # Force with no-prompt
+                -d  (or) --debug            # Enable Debug Mode\n"
     echo -e "\nExample\t\t: ./docker-local.sh -di testimage:latest -du docker_username -dp ******* -df ./Dockerfile -ds false -dpu false -dc ./context.txt -pt skip --force"
     echo; pattern_divider; echo && exit 1;
 }
